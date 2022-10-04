308cef6e111236f0fe1f99c86396044d4ebca204
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-10-04 18:41:18 +0400

    modal

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import './index.scss';
 import React from 'react';
+import './index.scss';
 
 function App() {
-  const [count, setCount] = React.useState(0)
+  const [isOpen, setIsOpen] = React.useState(false);
 
-  const addCount = () => {
-    setCount(count+1);
-  };
+  function openPopup() {
+    setIsOpen(true);
+  }
 
-  const deleteCount = () => {
-    setCount(count-1);
-  };
+  function closePopup() {
+    setIsOpen(false);
+  }
 
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button className="minus" onClick={deleteCount}>
-          - Минус
-        </button>
-        <button className="plus" onClick={addCount}>
-          Плюс +
-        </button>
+      <button className="open-modal-btn" onClick={openPopup}>
+        ✨ Открыть окно
+      </button>
+      <div className={`overlay animated ${isOpen ? 'show' : ''}`}>
+        <div className="modal">
+          <svg onClick={closePopup} height="200" viewBox="0 0 200 200" width="200">
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        </div>
       </div>
     </div>
   );
